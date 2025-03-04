fafb46059e34e7d4b40ada01eb77c037574137cc
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-03-04 15:00:48 +0100

    configuration settings for deployment

--- dan_diary/settings/common.py
+++ dan_diary/settings/common.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- dan_diary/settings/prod.py
+++ dan_diary/settings/prod.py
@@ -6,15 +6,17 @@ old settings.py file that comes with django by default.
 The aim of this file is to separate 'production' variable from development
 
 '''
-
+import dj_database_url
 from .common import *
 from decouple import config
 
 DEBUG = config('debug',default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dan-blog.onrender.com']
+
+DJANGO_SETTINGS_MODULE = 'dan_diary.settings.prod'
 
 
 DATABASES = {
-
+    'default': dj_database_url.parse(config('DB_URL'))
 }
