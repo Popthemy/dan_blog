41ab52aadd6b9a2cbb53c1f935b298fe1b9823c7
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2025-03-04 04:09:43 +0100

    urls: included drf-yasg url in root dir

--- dan_diary/urls.py
+++ dan_diary/urls.py
@@ -15,8 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Dan Diary: Blogging Platform",
+      default_version="v1",
+      description="Captures what intrigues me about life, science",
+    #   terms_of_service="",
+      contact=openapi.Contact(email="danny@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+
+] + debug_toolbar_urls()
